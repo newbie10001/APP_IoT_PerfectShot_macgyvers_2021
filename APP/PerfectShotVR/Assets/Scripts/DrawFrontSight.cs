0c2c8c5b52b3e2af7c59a1cf54e13c04a895feca
JadenChoi2k <jkya02@gmail.com>
2021-09-17 18:51:24 +0900

    Game Manager 생성 및 가늠쇠 이미지 추적 스크립트 작성 Game Manager에서 관리하는 속성들 목록  - 가늠쇠 활성화 여부  - 블루투스 활성화 여부  - 크리크 조정값  - 카메라 렌즈시프트 조정값  - 총 활성화 여부  - 응시모드 활성화 여부 가늠쇠 이미지 추적 스크립트 설명 : 총은 (0, 0, 0.1) 위치에서 가만히 있는다. 카메라가 움직이면 카메라 움직임에 따라 화면 상에서의 총 위치를 추적하여 가늠쇠 이미지를 갱신한다.

---
+++ APP/PerfectShotVR/Assets/Scripts/DrawFrontSight.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// 현재 가늠자가 어디 있는지 추적해서 UI에 갱신함.
+public class DrawFrontSight : MonoBehaviour
+{
+    // Gun 오브젝트 밑에 있는 Front Sight
+    // 실제 가늠자의 오브젝트
+    public GameObject frontSight;
+
+    private void Start()
+    {
+        Debug.Log($"가늠자 활성화 옵션 : {GameManager.instance.FrontSightEnabled}");
+        // 옵션 적용
+        this.gameObject.SetActive(GameManager.instance.FrontSightEnabled);
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(UpdateUI());
+    }
+
+    // 성능을 위해 0.5초 간격으로 실행
+    private IEnumerator UpdateUI()
+    {
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(frontSight.transform.position);
+        this.GetComponent<RectTransform>().position = screenPos;
+        yield return new WaitForSeconds(0.5f);
+        // false일 경우 루프가 멈춤.
+        if(GameManager.instance.FrontSightEnabled) StartCoroutine(UpdateUI());
+    }
+
+    // 설정창에서 토글하면 반응하는 메서드
+    public void SetActiveFrontSight(Toggle _toggle)
+    {
+        this.gameObject.SetActive(_toggle.isOn);
+    }
+}
