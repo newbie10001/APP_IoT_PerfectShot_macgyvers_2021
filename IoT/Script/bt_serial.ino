2bb98ce366b1c494b5c7ff82e89db024b20fe6d6
Sseung <45235528+sseungh@users.noreply.github.com>
2021-10-09 14:25:55 +0900

    bt_serial 모터드라이버, delay 삭제, 블루투스(write->println)

--- IoT/Script/bt_serial.ino
+++ IoT/Script/bt_serial.ino
@@ -1,26 +1,42 @@
 #include <SoftwareSerial.h>
 
-int vib_motor = 7;
-int BT_RX = 2;
-int BT_TX = 3;
-int FSR = A0;
+#define M1 6
+#define M2 7
+#define BT_RX 2
+#define BT_TX 3
+#define FSR 4
+int motor_loop = 0;
+bool vib = false;
 SoftwareSerial BTSerial(BT_RX, BT_TX);
 void setup() {
   // put your setup code here, to run once:
   BTSerial.begin(9600);
-  pinMode(vib_motor, OUTPUT);
-  pinMode(FSR, INPUT);
+  Serial.begin(9600);
+  pinMode(M1, OUTPUT);
+  pinMode(M2, OUTPUT);
+  pinMode(FSR, INPUT_PULLUP);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(BTSerial.available()){
-    float trig = analogRead(FSR);
-    if(trig > 500){
-      BTSerial.write("0");
-      digitalWrite(vib_motor, HIGH);
-      delay(300);
-      digitalWrite(vib_motor, LOW);
+  int trig = digitalRead(FSR);
+  if(trig == 0){
+    BTSerial.println("0");
+    vib = true;
+    motor_loop = 0;
+    //BTSerial.write("0");
+    //Serial.println(trig);
+  }
+  if(vib){
+    if(motor_loop++ == 0){
+      digitalWrite(M1, HIGH);
+      digitalWrite(M2, HIGH);
+    }else if(motor_loop > 500){
+      vib = false;
+      motor_loop = 0;
+      digitalWrite(M1, LOW);
+      digitalWrite(M2, LOW);
     }
   }
+  delay(10);
 }
